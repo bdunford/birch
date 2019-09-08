a4577550faf54a2abb16199b093a6b0130fca498
w1nds0r <birch.dunford@gmail.com>
2019-09-07 21:31:17 -0400

    Corrected by passing a max length into the merge function.

--- case4.c
+++ case4.c
@@ -7,8 +7,8 @@
 
 
 
-char* merge(char* a, char* b) {
-    char* ret = malloc(sizeof(a) + sizeof(b) + 1);
+char* merge(char* a, char* b, int len) {
+    char* ret = malloc(len + 1);
     int i = 0, ai = 0, bi=0;
     while (1) {
         if (a[ai] != '\0') {
@@ -24,6 +24,7 @@ char* merge(char* a, char* b) {
         }
 
         if (b[bi] == '\0' && a[ai] == '\0') {
+            i++;
             ret[i] = '\0';
             break;
         }
@@ -39,7 +40,7 @@ int main() {
     scanf("%[^\n]%*c",a);
     scanf("%[^\n]%*c",b);
 
-    char* merged = merge(a,b);
+    char* merged = merge(a,b,sizeof(a) + sizeof(b));
     printf("%s\n",merged);
     free(merged);
 }
